9391c5992695c3267586beef58e0eaaa29a3a54b
Tri Phan <phancaotri2212@gmail.com>
2023-11-06 15:18:19 +0700

    the power of 2

--- src/Ex02/Program.cs
+++ src/Ex02/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter number: ");
+
+            var num = int.Parse(Console.ReadLine());
+            var pow = Math.Pow(num, 2);
+
+            Console.WriteLine("The Result is: " + pow);
         }
     }
 }
