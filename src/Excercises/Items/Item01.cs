9ade503743aa1de507602b2f873bdaa8d153fe81
Quang Phan <quang.phan@tallygroup.com.au>
2023-11-07 12:24:06 +0700

    add generic items

---
+++ src/Excercises/Items/Item01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Items
+{
+    public class Item01 : BaseItem
+    {
+        public override string Description => "Write a C# Sharp program to print Hello and your name in a separate line";
+
+        public override void Run()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("John");
+        }
+    }
+}
