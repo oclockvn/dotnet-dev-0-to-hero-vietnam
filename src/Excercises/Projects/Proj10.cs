54e2947a1bdbe0fb39c8b825e1a45286c17c76c3
Tri Phan <phancaotri2212@gmail.com>
2023-11-07 16:31:39 +0700

    proj09

---
+++ src/Excercises/Projects/Proj10.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercises.Projects
+{
+    public class Proj10 : BaseProj
+    {
+        public override string Description => "Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z.";
+
+        public override void Run()
+        {
+            var x = GetNumber("Please enter the first number: ");
+            var y = GetNumber("Please enter the second number: ");
+            var z = GetNumber("Please enter the third number: ");
+
+            var result1 = (x + y) * z;
+            var result2 = (x * y) + (y * z);
+
+            Console.WriteLine($"Result of specified numbers {x}, {y} and {z}, ({x}+{y})*{z} is {result1} and {x}*{y} + {y}*{z} is {result2}");
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
