51fdd27a8c6c044a17c9a8ba7bb45b4889642153
Tri Phan <phancaotri2212@gmail.com>
2023-11-07 16:16:16 +0700

    proj07

---
+++ src/Excercises/Projects/Proj08.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercises.Projects
+{
+    public class Proj08 : BaseProj
+    {
+        public override string Description => "Write a C# Sharp program that prints the multiplication table of a number as input.";
+
+        public override void Run()
+        {
+            var firstNum = GetNumber("Please enter a number: ");
+
+            for (var i = 0; i <= 10; i++) 
+            {
+                Console.WriteLine($"{ firstNum}*{i} = {firstNum*i}");
+            }
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
