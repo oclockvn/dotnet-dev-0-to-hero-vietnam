fb34d89bdaf7a56357e2a4d109165fae3f21a066
Tri Phan <phancaotri2212@gmail.com>
2023-11-07 16:39:48 +0700

    proj11

---
+++ src/Excercises/Projects/Proj11.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj11 : BaseProj
+    {
+        public override string Description => "Write a C# Sharp program that takes an age (for example 20) as input and prints something like \"You look older than 20\"";
+
+        public override void Run()
+        {
+            var age = GetNumber("Please enter your age: ");
+            Console.WriteLine($"You look older than {Math.Abs(age)}");
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
