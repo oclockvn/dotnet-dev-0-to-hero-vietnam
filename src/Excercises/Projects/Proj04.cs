3d1c99fbe20f2bc6d3f75b69a53842f6f137cf90
Tri Phan <phancaotri2212@gmail.com>
2023-11-07 15:31:35 +0700

    proj04

---
+++ src/Excercises/Projects/Proj04.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    internal class Proj04 : BaseProj
+    {
+        public override string Description => "Write a C# Sharp program to print the results of the specified operations";
+
+        public override void Run()
+        {
+            Console.WriteLine(-1 + 4 * 6);
+        }
+    }
+}
