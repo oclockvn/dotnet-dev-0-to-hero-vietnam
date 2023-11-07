319cb502913e955818ae6927456fc5d0800abd28
Tri Phan <phancaotri2212@gmail.com>
2023-11-07 14:38:59 +0700

    ex11

---
+++ src/Ex11/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex11
+{
+    public class Program
+    {
+        // Write a C# Sharp program to print the result of dividing two numbers.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
