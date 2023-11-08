e070e74e4d104105b4d2799e448c921f3f468b11
Tri Phan <phancaotri2212@gmail.com>
2023-11-08 10:44:34 +0700

    proj16 17

---
+++ src/Excercises/Projects/Proj16.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj16 : BaseProj
+    {
+        public override string Description => "Write a C# program to create a new string from a given string where the first and last characters change their positions.";
+
+        public override void Run()
+        {
+            Console.WriteLine("Please enter a string: ");
+            var str = Console.ReadLine();
+
+            if (str.Length < 2)
+            {
+                Console.WriteLine($"The new string is {str}");
+
+            }
+            else if (str.Length == 2)
+            {
+                Console.WriteLine($"The new string is {str[1] + str[0]}");
+            }
+            else if (str.Length > 2)
+            {
+                var stringLeft = str.Substring(1, str.Length - 1);
+                Console.WriteLine($"The new string is {str[str.Length - 1] + stringLeft + str[0]}");
+            }
+        }
+    }
+}
---
+++ src/Excercises/Projects/Proj17.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj17 : BaseProj
+    {
+        public override string Description => "Write a C# program to create a string from a given string (length 1 or more) with the first character added at the front and back.";
+
+        public override void Run()
+        {
+            var str = GetString("Please enter a string: ");
+
+            if (str.Length < 1)
+            {
+                Console.WriteLine("You have entered anything yet.");
+            }
+            else if (str.Length >= 1)
+            {
+                var newStr = str.Substring(0, 1);
+                Console.WriteLine($"The new string is {newStr + str + newStr}");
+            }
+        }
+
+        static string GetString(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                var str = Console.ReadLine();
+                if (str != null)
+                {
+                    return str;
+                }
+            }
+        }
+    }
+}
