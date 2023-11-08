5e4d5c63055596c101091f2ee5b4ec12b028bcdf
Tri Phan <phancaotri2212@gmail.com>
2023-11-08 11:47:53 +0700

    proj23

---
+++ src/Excercises/Projects/Proj23.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercises.Projects
+{
+    public class Proj23 : BaseProj
+    {
+        public override string Description => "Write a C# program to convert a given string into lowercase.";
+
+        public override void Run()
+        {
+            Console.WriteLine("Please enter a string: ");
+            var str = Console.ReadLine();
+
+            if (str.Length >= 1)
+            {
+                Console.WriteLine(str.ToLower());
+            }
+            else { Console.WriteLine("Nothing happened!"); }
+        }
+    }
+}
