c9b26718d65fcc952f892e7bddc844cb8921efc7
Tri Phan <phancaotri2212@gmail.com>
2023-11-08 11:00:05 +0700

    proj 20

---
+++ src/Excercises/Projects/Proj19.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj19 : BaseProj
+    {
+        public override string Description => "Write a C# program to compute the sum of two given integers. If two values are the same, return the triple of their sum.";
+
+        public override void Run()
+        {
+            var num1 = GetNumber("Please enter the first number: ");
+            var num2 = GetNumber("Please enter the second number: ");
+
+            if (num1 != num2)
+            {
+                Console.WriteLine($"The sum of two number is: {num1 + num2}");
+            }
+            else
+            {
+                Console.WriteLine($"The result is {3 * (num1 + num2)}");
+            }
+        }
+
+        static int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
---
+++ src/Excercises/Projects/Proj20.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj20 : BaseProj
+    {
+        public override string Description => "Write a C# program to get the absolute value of the difference between two given numbers. Return double the absolute value of the difference if the first number is greater than the second number.";
+
+        public override void Run()
+        {
+            var num1 = GetNumber("Please enter the first number: ");
+            var num2 = GetNumber("Please enter the second number: ");
+
+            var result = Math.Abs(num1 - num2);
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"The result is {2 * (num1 - num2)}");
+            }
+            else
+            {
+                Console.WriteLine($"The result is {num2 - num1}");
+            }
+        }
+
+        static int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
