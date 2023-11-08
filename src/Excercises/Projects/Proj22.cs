497a0e26b9a12a8dcfc8f27551ea722239f8aede
Tri Phan <phancaotri2212@gmail.com>
2023-11-08 11:35:32 +0700

    proj22

---
+++ src/Excercises/Projects/Proj22.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj22 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if the given integer is within 20 of 100 or 200.";
+
+        public override void Run()
+        {
+            var num = GetNumber("Please enter a number: ");
+
+            if (Math.Abs(num - 100) <= 20 || Math.Abs(num - 200) <= 20)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
