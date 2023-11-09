db2cb46629417b44e2f8d33803cb8e80657d2f57
Tri Phan <phancaotri2212@gmail.com>
2023-11-09 12:18:32 +0700

    ex28

---
+++ src/Excercises/Projects/Proj28.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj28 : BaseProj
+    {
+        public override string Description => "Write a C# program to reverse the words of a sentence.";
+
+        public override void Run()
+        {
+            Console.WriteLine("Please enter the sentence: ");
+            var sen = Console.ReadLine();
+            var result = "";
+            var words = sen.Split(new[] { ' ' }, StringSplitOptions.None);
+
+            if (!sen.Contains(" "))
+            {
+                Console.WriteLine(sen);
+            }
+
+            var len = words.Length;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                result += words[i] + " "; 
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ src/Excercises/Projects/Proj29.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj29 : BaseProj
+    {
+        public override string Description => "";
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
