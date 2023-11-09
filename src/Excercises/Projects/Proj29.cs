ba744efcb2ce1d24779ec32417dad8c8fcd364cc
Tri Phan <phancaotri2212@gmail.com>
2023-11-09 12:39:46 +0700

    proj29

--- src/Excercises/Projects/Proj29.cs
+++ src/Excercises/Projects/Proj29.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercises.Projects
+﻿namespace Exercises.Projects
 {
     public class Proj29 : BaseProj
     {
-        public override string Description => "";
+        public override string Description => "Write a C# program to find the size of a specified file in bytes.";
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            var path = GetPath("Please enter a file path: ");
+            var fileInfo = new FileInfo(path);
+            var size = fileInfo.Length;
+
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine($"The size of file in bytes is : {size}");
+            }
+            else
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+        }
+
+        public string GetPath(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                var path = Console.ReadLine();
+                if (path.Length != 0)
+                {
+                    return path;
+                }
+            }
         }
     }
 }
