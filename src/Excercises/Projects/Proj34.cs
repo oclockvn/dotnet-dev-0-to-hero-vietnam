9d45c7505a9f273c5dcc5b73f720490f4a0cc470
Tri Phan <phancaotri2212@gmail.com>
2023-11-13 14:59:46 +0700

    proj34

---
+++ src/Excercises/Projects/Proj34.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj34 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if a string starts with a specified word. Note: Suppose the sentence starts with \"Hello\"";
+
+        public override void Run()
+        {
+            Console.Write("Please enter a string: ");
+            var str = Console.ReadLine();
+
+
+        }
+    }
+}
