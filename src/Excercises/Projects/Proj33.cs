b2f4e7cb92e7b843421e517a45e6f2d2129af390
Tri Phan <phancaotri2212@gmail.com>
2023-11-13 14:56:33 +0700

    proj33

---
+++ src/Excercises/Projects/Proj33.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj33 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if a given positive number is a multiple of 3 or 7.";
+
+        public override void Run()
+        {
+            var num = GetNumber("Please enter a numbre: ");
+            Console.WriteLine(CheckNumber(num));
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+
+        public bool CheckNumber(int number)
+        {
+            if (number%3 == 0 || number%7==0)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
