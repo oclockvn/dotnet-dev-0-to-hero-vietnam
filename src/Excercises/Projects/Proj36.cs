ba9485fd13eeffab62b7b21c9954c0025f6c6656
Tri Phan <phancaotri2212@gmail.com>
2023-11-14 14:25:51 +0700

    proj36

---
+++ src/Excercises/Projects/Proj36.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj36 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if an integer (from the two given integers) is in the range -10 to 10.";
+
+        public override void Run()
+        {
+            var num1 = GetNumber("Please enter the first number: ");
+            var num2 = GetNumber("Please enter the second number: ");
+
+            var check = CheckNumber(num1, num2);
+            Console.WriteLine(check);
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+
+        public bool CheckNumber(int num1, int num2)
+        {
+            if ((-10 <= num1 && num1 >= 10) || (-10 <= num2 && num2 >= 10))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
