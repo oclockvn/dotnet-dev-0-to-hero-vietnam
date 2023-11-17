02c763386d13fd4dc898cebd08d15cf9bb73ddcb
Tri Phan <phancaotri2212@gmail.com>
2023-11-17 12:13:24 +0700

    proj47

---
+++ src/Excercises/Projects/Proj47.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercises.Projects
+{
+    public class Proj47 : BaseProj
+    {
+        public override string Description => "Write a C# program to compute the sum of all the elements of an array of integers.";
+
+        public override void Run()
+        {
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sum = 0;
+
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
