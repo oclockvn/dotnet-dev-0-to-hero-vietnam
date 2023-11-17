5a7569fed1ca16f4fda2573d0f2847340b0fe4bf
Tri Phan <phancaotri2212@gmail.com>
2023-11-17 11:11:00 +0700

    proj43

---
+++ src/Excercises/Projects/Proj43.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj43 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if a given string starts with \"w\" and is immediately followed by two \"ww\".";
+
+        public override void Run()
+        {
+            var str = GetString("Please enter a string: ");
+
+            if (str.ToLower()[0] != 'w')
+            {
+                Console.WriteLine(false);
+            }
+            else if (str.ToLower()[0] == 'w' && str.Substring(1,2).ToLower() == "ww")
+            {
+                Console.WriteLine(false);
+            }
+            else
+            {
+                Console.WriteLine(true);
+            }
+        }
+
+        public string GetString(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                var str = Console.ReadLine();
+                if (str.Length > 0)
+                {
+                    return str;
+                }
+            }
+        }
+    }
+}
