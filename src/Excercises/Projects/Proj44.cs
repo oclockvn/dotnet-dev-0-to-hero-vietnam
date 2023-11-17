b6bfd63ccd48fd04c033a7d27156d41396dd1acf
Tri Phan <phancaotri2212@gmail.com>
2023-11-17 11:20:12 +0700

    proj44

---
+++ src/Excercises/Projects/Proj44.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj44 : BaseProj
+    {
+        public override string Description => "Write a C# program to create a string of every other character (odd position) from the first position of a given string.";
+
+        public override void Run()
+        {
+            var str = GetString("Please enter a string: ");
+            var newStr = CreateNewString(str);
+            Console.WriteLine(newStr);
+        }
+
+        public string GetString(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                var str = Console.ReadLine();
+                if (str.Length > 0)
+                {
+                    return str;
+                }
+            }
+        }
+
+        public string CreateNewString(string str)
+        {
+            var newStr = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    newStr += str[i];
+                }
+            }
+
+            return newStr;
+        }
+    }
+}
