b421121a618f2d8c1c261ab099a2a701cc93e9d5
Tri Phan <phancaotri2212@gmail.com>
2023-11-17 10:42:18 +0700

    proj 42

---
+++ src/Excercises/Projects/Proj41.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj41 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if a given string contains the 'w' character between 1 and 3 times.";
+
+        public override void Run()
+        {
+            var str = GetString("Please enter a string: ");
+            var check = CheckString(str);
+
+            Console.WriteLine(check);
+        }
+
+        public string GetString(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                var str = Console.ReadLine();
+
+                if (str != null)
+                {
+                    return str;
+                }
+            }
+        }
+
+        public bool CheckString(string str)
+        {
+            var count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.ToLower()[i] == 'w')
+                {
+                    count++;
+                }
+            }
+
+            if (count >= 1 && count <= 3)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Excercises/Projects/Proj42.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Projects
+{
+    public class Proj42 : BaseProj
+    {
+        public override string Description => "Write a C# program to create a string where the first 4 characters are in lower case. If the string is less than 4 letters, make the whole string in upper case.";
+
+        public override void Run()
+        {
+            var str = GetString("Please enter a string: ");
+
+            var newStr = CreateNewString(str);
+            Console.WriteLine(newStr);
+        }
+
+        public string GetString(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                var str = Console.ReadLine();
+                if (str.Length > 0)
+                {
+                    return str;
+                }
+            }
+        }
+
+        public string CreateNewString(string str)
+        {
+            if (str.Length <= 4)
+            {
+                return str.ToUpper();
+            }
+            else
+            {
+                return str.Substring(0, 4).ToLower() + str.Substring(4,str.Length - 4);
+            }
+        }
+    }
+}
