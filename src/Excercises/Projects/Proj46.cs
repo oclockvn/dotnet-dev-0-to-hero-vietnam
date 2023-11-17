c668895b9303e9f1c435d19634b81a512b61f0d2
Tri Phan <phancaotri2212@gmail.com>
2023-11-17 12:09:07 +0700

    proj46

---
+++ src/Excercises/Projects/Proj46.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercises.Projects
+{
+    public class Proj46 : BaseProj
+    {
+        public override string Description => "Write a C# program to check if a number appears as the first or last element of an array of integers. The array length is 1 or more.";
+
+        public override void Run()
+        {
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var num = GetNumber("Please enter a number: ");
+            if ((num == arr[0]) || (num == arr[arr.Length - 1]))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+
+        }
+
+        public int GetNumber(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out var number))
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
