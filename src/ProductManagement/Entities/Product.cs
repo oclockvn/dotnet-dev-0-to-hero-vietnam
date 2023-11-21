13d6c28f04fab785db3c91dff340f26c83835785
Tri Phan <phancaotri2212@gmail.com>
2023-11-21 16:51:56 +0700

    product entity

--- src/ProductManagement/Entities/Product.cs
+++ src/ProductManagement/Entities/Product.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ProductManagement.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quatity { get; set; }
     }
 }
