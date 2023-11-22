373e567456624c69abfc6e2b419896717304b315
Tri Phan <phancaotri2212@gmail.com>
2023-11-22 11:25:52 +0700

    db

---
+++ src/ProductMangement/Migration/database.sql
@@ -0,0 +1,12 @@
+﻿create database ProductMangement
+go
+use ProductMangement
+go
+create table Products (
+Id int identity (1,1) primary key,
+[Name] nvarchar (255) not null,
+Price decimal not null default(0),
+Quantity int not null default(0),
+)
+
+
