8050ee360fc84bb930c64a187e9c18e09e45de3a
Tri Phan <phancaotri2212@gmail.com>
2023-11-22 16:28:40 +0700

    oop2

---
+++ src/OOP-Ex02/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OOP_Ex02;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount ba = new BankAccount();
+        ba.Depositing(200);
+        ba.Depositing(120);
+
+        ba.Withdrawing(20);
+        ba.CheckBalance();
+
+        var ba2 = new BankAccount();
+        ba2.Depositing(100);
+    }
+}
+
+public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public decimal Balance { get; set; }
+    public string AccountHolderName { get; set; } = null!;
+
+    public void Depositing(decimal num)
+    {
+        Balance += num;
+    }
+
+    public void Withdrawing(decimal num)
+    {
+        Balance -= num;
+    }
+
+    public void CheckBalance()
+    {
+        Console.WriteLine($"{Balance}");
+    }
+}
