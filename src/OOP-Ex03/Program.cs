23a72b56f03182287408b003e0fcede638b6bd6c
Tri Phan <phancaotri2212@gmail.com>
2023-11-22 20:33:58 +0700

    oop3

---
+++ src/OOP-Ex03/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace OOP_Ex03;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius) { }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) { }
+
+    public override double CalculateArea()
+    {
+        return length * width;
+    }
+}
+
+public class Triangle : Shape
+{
+    private double _base;
+    private double height;
+
+    public Triangle(double _base, double height) { }
+
+    public override double CalculateArea()
+    {
+        throw new NotImplementedException();
+    }
+}
