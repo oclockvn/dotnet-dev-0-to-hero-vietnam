d7ae4c6984257c5ab3c185de3adf3d40d258c728
Tri Phan <phancaotri2212@gmail.com>
2023-11-22 10:42:51 +0700

    implement ui

--- src/ProductManagement/Services/ProductService.cs
+++ src/ProductManagement/Services/ProductService.cs
@@ -1,12 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Entities;
+using ProductManagement.Models;
 
 namespace ProductManagement.Services
 {
-    internal class ProductService
+    interface IProductService
     {
+        Task<List<ProductModel>> GetProductsAsync();
+        Task<ProductModel?> GetProductAsync(int id);
+        Task<ProductModel> AddProductAsync(ProductModel p);
+        Task UpdateProductAsync(ProductModel p);
+        Task DeleteProductAsync(int id);
+    }
+
+    public class ProductService : IProductService
+    {
+        private ApplicationDbContext db = new();
+
+        public async Task<List<ProductModel>> GetProductsAsync()
+        {
+            var products = await db.Products.Select(x => new ProductModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Quatity = x.Quatity,
+            }).ToListAsync();
+
+            return products;
+        }
+
+        public async Task<ProductModel?> GetProductAsync(int id)
+        {
+            var product = await db.Products
+                .Where(x => x.Id == id)
+                .Select(x => new ProductModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Quatity = x.Quatity,
+                }).FirstOrDefaultAsync();
+
+            return product;
+        }
+
+        public async Task<ProductModel> AddProductAsync(ProductModel p)
+        {
+            Product product = new();
+            product.Name = p.Name;
+            product.Price = p.Price;
+            product.Quatity = p.Quatity;
+
+            db.Products.Add(product);
+            var save = await db.SaveChangesAsync();
+
+            if (save == 0)
+            {
+                throw new Exception("Cannot save change.");
+            }
+
+            return new ProductModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quatity = product.Quatity,
+            };
+        }
+
+        public async Task UpdateProductAsync(ProductModel p)
+        {
+            var product = await db.Products.FirstOrDefaultAsync(x => x.Id == p.Id);
+
+            if (product == null)
+            {
+                throw new Exception($"No product found by Id {p.Id}");
+            }
+
+            product.Name = p.Name;
+            product.Price = p.Price;
+            product.Quatity = p.Quatity;
+
+            var save = await db.SaveChangesAsync();
+
+            if (save == 0)
+            {
+                throw new Exception("Cannot save change.");
+            }
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await db.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                throw new Exception("This product doesn't exist.");
+            }
+
+            db.Products.Remove(product);
+            var save = await db.SaveChangesAsync();
+
+            if (save == 0)
+            {
+                throw new Exception("Cannot save change.");
+            }
+        }
     }
 }
--- src/ProductMangement/Form1.Designer.cs
+++ src/ProductMangement/Form1.Designer.cs
@@ -28,12 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lvProduct = new ListView();
+            gbProduct = new GroupBox();
+            colID = new ColumnHeader();
+            colName = new ColumnHeader();
+            colPrice = new ColumnHeader();
+            colQuantity = new ColumnHeader();
+            lbName = new Label();
+            txtName = new TextBox();
+            txtQuantity = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtPrice = new TextBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            btnDelete = new Button();
+            btnUpdate = new Button();
+            gbProduct.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lvProduct
+            // 
+            lvProduct.Columns.AddRange(new ColumnHeader[] { colID, colName, colPrice, colQuantity });
+            lvProduct.FullRowSelect = true;
+            lvProduct.GridLines = true;
+            lvProduct.Location = new Point(12, 50);
+            lvProduct.Name = "lvProduct";
+            lvProduct.Size = new Size(456, 246);
+            lvProduct.TabIndex = 0;
+            lvProduct.UseCompatibleStateImageBehavior = false;
+            lvProduct.View = View.Details;
+            // 
+            // gbProduct
+            // 
+            gbProduct.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            gbProduct.BackColor = Color.Silver;
+            gbProduct.Controls.Add(btnCancel);
+            gbProduct.Controls.Add(btnSave);
+            gbProduct.Controls.Add(txtPrice);
+            gbProduct.Controls.Add(txtQuantity);
+            gbProduct.Controls.Add(label2);
+            gbProduct.Controls.Add(label1);
+            gbProduct.Controls.Add(txtName);
+            gbProduct.Controls.Add(lbName);
+            gbProduct.Location = new Point(489, 50);
+            gbProduct.Name = "gbProduct";
+            gbProduct.Size = new Size(233, 246);
+            gbProduct.TabIndex = 1;
+            gbProduct.TabStop = false;
+            gbProduct.Text = "New Product";
+            // 
+            // colID
+            // 
+            colID.Text = "ID";
+            colID.Width = 30;
+            // 
+            // colName
+            // 
+            colName.Text = "Product Name";
+            colName.Width = 200;
+            // 
+            // colPrice
+            // 
+            colPrice.Text = "Price";
+            colPrice.Width = 100;
+            // 
+            // colQuantity
+            // 
+            colQuantity.Text = "Quantity";
+            colQuantity.Width = 100;
+            // 
+            // lbName
+            // 
+            lbName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lbName.AutoSize = true;
+            lbName.Location = new Point(6, 36);
+            lbName.Name = "lbName";
+            lbName.Size = new Size(84, 15);
+            lbName.TabIndex = 0;
+            lbName.Text = "Product Name";
+            // 
+            // txtName
+            // 
+            txtName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtName.Location = new Point(6, 54);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(221, 23);
+            txtName.TabIndex = 1;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtQuantity.Location = new Point(6, 142);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.Size = new Size(221, 23);
+            txtQuantity.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Quantity";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            txtPrice.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtPrice.Location = new Point(6, 98);
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new Size(221, 23);
+            txtPrice.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(6, 193);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(108, 23);
+            btnSave.TabIndex = 4;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCancel.Location = new Point(120, 193);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(107, 23);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnDelete.Location = new Point(361, 302);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(107, 23);
+            btnDelete.TabIndex = 6;
+            btnDelete.Text = "Delete Product";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnUpdate.Location = new Point(276, 302);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(79, 23);
+            btnUpdate.TabIndex = 7;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(734, 350);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(gbProduct);
+            Controls.Add(lvProduct);
+            Name = "Form1";
+            Text = "Form1";
+            gbProduct.ResumeLayout(false);
+            gbProduct.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListView lvProduct;
+        private GroupBox gbProduct;
+        private ColumnHeader colID;
+        private ColumnHeader colName;
+        private ColumnHeader colPrice;
+        private ColumnHeader colQuantity;
+        private TextBox txtPrice;
+        private TextBox txtQuantity;
+        private Label label2;
+        private Label label1;
+        private TextBox txtName;
+        private Label lbName;
+        private Button btnCancel;
+        private Button btnSave;
+        private Button btnDelete;
+        private Button btnUpdate;
     }
 }
