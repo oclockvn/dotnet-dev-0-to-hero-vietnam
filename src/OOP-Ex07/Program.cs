f97a10984437f754edc41e4481ce8c9c6489640f
Tri Phan <phancaotri2212@gmail.com>
2023-11-24 10:01:03 +0700

    account bank

---
+++ src/OOP-Ex07/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Threading.Channels;
+
+namespace OOP_Ex07;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Account a = new(10);
+        a.Deposit(20);
+        a.Withdraw(5);
+        Console.WriteLine(a.Balance);
+        
+    }
+}
+
+public class Account
+{
+    public string HolderName { get; set; } = null!;
+    public string Type { get; private set; } = null!;
+    public double Balance { get; private set; }
+
+    public Account(string holderName, string type, double balance)
+    {
+        HolderName = holderName;
+        Type = type;
+        Balance = balance;
+    }
+
+    public Account(double balance)
+    {
+        Balance = balance;
+    }
+
+    public double Deposit(double amount)
+    {
+        Balance += amount;
+        return Balance;
+    }
+
+    public double Withdraw(double amount)
+    {
+        Balance -= amount;
+        return Balance;
+    }
+
+    public void CheckBalance()
+    {
+        Console.WriteLine($"The balance is {Balance}");
+    }
+}
+
+public class ATM
+{
+    private List<Account> accounts = new List<Account>();
+    
+    public void CreateAccount(string holdername, string type, double balance)
+    {
+        Account newAcc = new Account(holdername, type, balance);
+        accounts.Add(newAcc);
+    }
+
+    public void SelectAccount(string holdername)
+    {
+        var selectAcc = accounts.Find(x => x.HolderName == holdername);
+        AccountTransaction(selectAcc);
+    }
+
+    public void AccountTransaction(Account selectAcc)
+    {
+        int option;
+        do
+        {
+            Console.WriteLine("1.Deposit");
+            Console.WriteLine("2.Withdraw");
+            Console.WriteLine("3.Check Balance");
+            Console.WriteLine("0.Exit");
+
+            option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter deposit amount: ");
+                    var depositAmount = double.Parse(Console.ReadLine());
+                    selectAcc.Deposit(depositAmount);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter withdraw amount: ");
+                    var withdrawAmount = double.Parse(Console.ReadLine());
+                    selectAcc.Withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    selectAcc.CheckBalance();
+                    break;
+            }
+
+        } while (option != 0);
+    }
+}
