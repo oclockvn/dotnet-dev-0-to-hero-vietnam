6baba2b8bc134c40c4891645474957e8d84ccf15
Tri Phan <phancaotri2212@gmail.com>
2023-11-24 16:28:22 +0700

    whistle

---
+++ src/OOP-Ex12-Whistle/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP_Ex12_Whistle;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var whistle = new Whistle("beef");
+
+        whistle.Sound();
+    }
+}
+
+public class Whistle
+{
+    private readonly string whistleSound;
+
+    public Whistle(string whistleSound)
+    {
+        this.whistleSound = whistleSound;
+    }
+
+    public void Sound()
+    {
+        Console.WriteLine(whistleSound);
+    }
+}
