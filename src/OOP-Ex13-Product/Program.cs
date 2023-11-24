301ca59ba2bb5373620eb7753dd9e7467e11a902
Tri Phan <phancaotri2212@gmail.com>
2023-11-24 16:43:42 +0700

    oop 13

---
+++ src/OOP-Ex13-Product/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OOP_Ex13_Product;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var product = new Product("Banana", 1.1, 13);
+
+        product.PrintProduct();
+    }
+}
+
+public class Product
+{
+    private readonly string name;
+    private readonly double price;
+    private readonly int quantity;
+
+    public Product(string name, double price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void PrintProduct()
+    {
+        Console.WriteLine($"{name} : {price} : {quantity} pcs");
+    }
+}
