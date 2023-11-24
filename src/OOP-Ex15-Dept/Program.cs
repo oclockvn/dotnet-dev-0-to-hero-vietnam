8046beddfe1f9ac4732f0e4ff1efbd2cde4f1e5e
Tri Phan <phancaotri2212@gmail.com>
2023-11-24 23:26:59 +0700

    dept

---
+++ src/OOP-Ex15-Dept/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP_Ex15_Dept;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Dept dept = new Dept(120000, 1.01);
+        dept.PrintBalance();
+
+        dept.WaitOneYear();
+        dept.PrintBalance();
+
+        var years = 0;
+        while (years < 20)
+        {
+            dept.WaitOneYear();
+            years++;
+        }
+
+        dept.PrintBalance();
+    }
+}
+
+public class Dept
+{
+    private double balance;
+    private double interestRate;
+
+    public Dept(double initialBalance, double initialInterestRate)
+    {
+        this.balance = initialBalance;
+        this.interestRate = initialInterestRate;
+    }
+
+    public void PrintBalance()
+    {
+        Console.WriteLine(balance);
+    }
+
+    public void WaitOneYear()
+    {
+        balance = balance * interestRate;
+    }
+}
