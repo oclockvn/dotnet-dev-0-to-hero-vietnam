c0bc6cd65470260f2ec05b20bc95cb3423d84ce4
Tri Phan <phancaotri2212@gmail.com>
2023-11-24 23:13:54 +0700

    counter

---
+++ src/OOP-Ex14-Counter/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace OOP_Ex14_Counter;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        DecreasingCounter counter = new DecreasingCounter(20);
+        counter.PrintValue();
+
+        counter.ResetValue();
+        counter.PrintValue();
+    }
+}
+
+public class DecreasingCounter
+{
+    private int value;
+
+    public DecreasingCounter(int initialValue)
+    {
+        this.value = initialValue;
+    }
+
+    public void PrintValue()
+    {
+        Console.WriteLine("Value: " + this.value);
+    }
+
+    public void Decrement()
+    {
+        if (value > 0)
+        {
+            value--;
+        }
+    }
+
+    public void ResetValue()
+    {
+        value = 0;
+    }
+}
