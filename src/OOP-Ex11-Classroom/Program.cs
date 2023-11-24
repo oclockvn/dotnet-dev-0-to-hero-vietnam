c84c9d4d5cf16d83f5f22d6b87e17d2283f7cd41
Tri Phan <phancaotri2212@gmail.com>
2023-11-24 16:21:59 +0700

    classsroom

---
+++ src/OOP-Ex11-Classroom/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_Ex11_Classroom;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Classroom
+{
+    private string code;
+    private int seat;
+
+    public Classroom(string classCode, int numberOfSeat) : this(numberOfSeat)
+    {
+        code = classCode;
+    }
+
+    public Classroom(int numberOfSeat)
+    {
+        seat = numberOfSeat;
+    }
+}
