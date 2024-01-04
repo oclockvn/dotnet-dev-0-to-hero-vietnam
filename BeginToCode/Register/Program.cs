c4435f0161bbb005c4804be546833a3884704d8e
LucasPhan <phancaotri2212@gmail.com>
2024-01-04 09:50:03 +0700

    ex

---
+++ BeginToCode/Register/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Register;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your email: ");
+        var enteredEmail = Console.ReadLine();
+
+        if (IsEmailUnique(enteredEmail))
+        {
+            Console.WriteLine("Please enter your password: ");
+            var enteredPassword = Console.ReadLine();
+
+            var passwordValidationResult = IsValidPassword(enteredPassword);
+            if (passwordValidationResult.valid)
+            {
+                RegisterUser(new Register { Email = enteredEmail, Password = enteredPassword });
+                Console.WriteLine("Registration successful!");
+            }
+            else
+            {
+                Console.WriteLine($"Password validation failed: {passwordValidationResult.error}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Email is already registered.Please use a different email.");
+        }
+    }
+
+    class Register
+    {
+        public string Email {  get; set; }
+        public string Password { get; set; }
+    }
+
+    static List<string> registerEmail = new();
+
+    static (bool valid, string? error) IsValidPassword(string password,
+        int min = 6,
+        int max = 20,
+        bool requiresSpecialCharacters = true,
+        bool requiresLowercaseUpperCase = true,
+        bool requiresNumber = true)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            return (false, "Password must not empty");
+        }
+
+        if (password.Length < min)
+        {
+            return (false, $"Password must have minimum {min} character");
+        }
+
+        if (password.Length > max)
+        {
+            return (false, $"Password must have maximum {max} character");
+        }
+
+        if (requiresSpecialCharacters)
+        {
+            if (!Regex.IsMatch(password, @".*[^a-zA-Z0-9].*"))
+                return (false, "Password must have special characters");
+        }
+
+        if (requiresLowercaseUpperCase)
+        {
+            if (!Regex.IsMatch(password, "[a-z]") || !Regex.IsMatch(password, "[A-Z]"))
+                return (false, "Password must have lowercase and uppercase");
+        }
+
+        if (requiresNumber)
+        {
+            if (!Regex.IsMatch(password, ".*\\d.*"))
+                return (false, "Password must have numbers");
+        }
+
+        return (true, null);
+    }
+
+    static bool IsEmailUnique(string email)
+    {
+        return !registerEmail.Contains(email);
+    }
+
+    static void RegisterUser(Register user)
+    {
+        registerEmail.Add(user.Email);
+    }
+}
--- BeginToCode/w17_Method_Library/Program.cs
+++ BeginToCode/w17_Method_Library/Program.cs
@@ -19,42 +19,42 @@
                     Console.WriteLine("Invalid password");
                 }
             } while (true);
-            
+
         }
 
         static bool IsValidPassword(string password)
         {
-                if (password == null)
-                {
-                    Console.WriteLine("Please enter password");
-                    return false;
-                }
+            if (string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Please enter password");
+                return false;
+            }
 
-                if (password.Length < 6)
-                {
-                    Console.WriteLine("Password must be at least 6 characters");
-                    return false;
-                }
-                else if (password.Length > 20)
-                {
-                    Console.WriteLine("Password must have maximum 20 characters");
-                }
+            if (password.Length < 6)
+            {
+                Console.WriteLine("Password must be at least 6 characters");
+                return false;
+            }
+            else if (password.Length > 20)
+            {
+                Console.WriteLine("Password must have maximum 20 characters");
+            }
 
-                // Password must have number or special characters
-                if (!password.Any(char.IsDigit) && !password.Any(char.IsPunctuation))
-                {
-                    Console.WriteLine("Password must have number or special characters");
-                    return false;
-                }
+            // Password must have number or special characters
+            if (!password.Any(char.IsDigit) && !password.Any(char.IsPunctuation))
+            {
+                Console.WriteLine("Password must have number or special characters");
+                return false;
+            }
 
-                // Password must have lowercase and uppercase characters
-                if (!password.Any(char.IsLower) || !password.Any(char.IsUpper))
-                {
-                    Console.WriteLine("Password must have lowercase and uppercase characters");
-                    return false;
-                }
+            // Password must have lowercase and uppercase characters
+            if (!password.Any(char.IsLower) || !password.Any(char.IsUpper))
+            {
+                Console.WriteLine("Password must have lowercase and uppercase characters");
+                return false;
+            }
 
-                return true;
+            return true;
         }
     }
 }
